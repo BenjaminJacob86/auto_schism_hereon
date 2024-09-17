9ca4f8f2d16b2c5d82aa0937972c8df4415dfb25
BenjaminJacob86 <88046352+BenjaminJacob86@users.noreply.github.com>
2024-09-17 16:07:19 +0200

    make a hard code command test due to error

--- templates/sim.sh
+++ templates/sim.sh
@@ -8,10 +8,26 @@ CONTAINER_LOCATION="/gpfs/scratch/nct_306/schism_arg.sif"
 CONTAINER_ARGS_PRE="--bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output" # this is path bindings in our case
 CONTAINER_ARGS_POST="4"   #this is the number of scribes
 
-mpirun -np 8 singularity run \
+#I get error message via auto submit:
+#
+#mpirun -np 8 singularity run \
+#  "${CONTAINER_ARGS_PRE}"  \
+#  "${CONTAINER_LOCATION}" \
+#      ${CONTAINER_ARGS_POST}
+#+ mpirun -np 8 singularity run '--bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output' /gpfs/scratch/nct_306/schism_arg.sif 4
+#Error for command "run": unknown flag: --bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output
+#
+
+# therefore I try hard coded command
+
+#mpirun -np 8 singularity run \
   "${CONTAINER_ARGS_PRE}"  \
   "${CONTAINER_LOCATION}" \
       ${CONTAINER_ARGS_POST}
+	  
+# hard code test	  
+mpirun -np 8 singularity run --bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output schism_arg.sif 4
+	  
 
 echo "OK!"
 
