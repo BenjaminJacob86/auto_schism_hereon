cb4a4896c9472a7d7df1a1ee97f026e47b3edc92
BenjaminJacob86 <88046352+BenjaminJacob86@users.noreply.github.com>
2024-09-17 16:52:43 +0200

    fix woirking directory (should only be a temporal solution)

--- templates/sim.sh
+++ templates/sim.sh
@@ -47,5 +47,10 @@ CONTAINER_LOCATION="/gpfs/scratch/nct_306/schism_arg.sif"
 CONTAINER_ARGS_PRE="--bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output"
 CONTAINER_ARGS_POST="/SCHISM/GBconfig/bin/pschism_PREC_EVAP_TVD-SB 4"
 
+
+# Without the --pwd /rundir option, the container would execute in the default working directory set by Autosubmit, which is likely the experiment folder (e.g., a000, a001, etc.). Since your Singularity container expects to find certain files (like input files, namelists, etc.) in /rundir, it failed to locate them when running in the experiment folder.
+
+# Workaround
+
 # Run the job (add --pwd to ensure /rundir is set as the working directory)
 mpirun -np 8 singularity exec --pwd /rundir ${CONTAINER_ARGS_PRE} ${CONTAINER_LOCATION} ${CONTAINER_ARGS_POST}
