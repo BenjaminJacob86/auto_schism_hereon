8d84b1c75a06c7b31ee3d99c4b1ebc997c044afc
BenjaminJacob86 <88046352+BenjaminJacob86@users.noreply.github.com>
2024-09-17 16:15:11 +0200

    test again

--- templates/sim.sh
+++ templates/sim.sh
@@ -4,9 +4,9 @@ module load singularity
 
 #CONTAINER_LOCATION="/gpfs/scratch/nct_306/schism_arg.sif"
 #CONTAINER_ARGS="--bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output schism_arg.sif 4"
-CONTAINER_LOCATION="/gpfs/scratch/nct_306/schism_arg.sif"
-CONTAINER_ARGS_PRE="--bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output" # this is path bindings in our case
-CONTAINER_ARGS_POST="4"   #this is the number of scribes
+#CONTAINER_LOCATION="/gpfs/scratch/nct_306/schism_arg.sif"
+#CONTAINER_ARGS_PRE="--bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output" # this is path bindings in our case
+#:qCONTAINER_ARGS_POST="4"   #this is the number of scribes
 
 #I get error message via auto submit:
 #
@@ -21,9 +21,9 @@ CONTAINER_ARGS_POST="4"   #this is the number of scribes
 # therefore I try hard coded command
 
 #mpirun -np 8 singularity run \
-  "${CONTAINER_ARGS_PRE}"  \
-  "${CONTAINER_LOCATION}" \
-      ${CONTAINER_ARGS_POST}
+ # "${CONTAINER_ARGS_PRE}"  \
+ # "${CONTAINER_LOCATION}" \
+ #     ${CONTAINER_ARGS_POST}
 	  
 # hard code test	  
 mpirun -np 8 singularity run --bind /gpfs/scratch/nct_306/nct01095/BSideal:/rundir,/gpfs/scratch/nct_306/nct01095/BSideal/input:/input,/gpfs/scratch/nct_306/nct01095/BSideal/outputs:/output schism_arg.sif 4
